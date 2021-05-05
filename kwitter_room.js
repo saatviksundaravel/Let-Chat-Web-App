bdfb0c50fdd588e9d54fdb0309f7831004266059
saatviksundaravel <77335691+saatviksundaravel@users.noreply.github.com>
2021-05-05 20:13:08 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,1 +1,16 @@
-//Kwitter.js
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCgDcRzJQovwfygPEaDNkIafp82RGAploY",
+    authDomain: "let-chat-web-app-220f5.firebaseapp.com",
+    databaseURL: "https://let-chat-web-app-220f5-default-rtdb.firebaseio.com",
+    projectId: "let-chat-web-app-220f5",
+    storageBucket: "let-chat-web-app-220f5.appspot.com",
+    messagingSenderId: "29419020013",
+    appId: "1:29419020013:web:d66d30567b984808c2b6e4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  function addRoom(){
+    
+  }
