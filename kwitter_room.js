4e3cd64fe62edcff5b54c1108154c3b80a57abe4
saatviksundaravel <77335691+saatviksundaravel@users.noreply.github.com>
2021-05-13 19:09:00 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,16 +1,49 @@
- // Your web app's Firebase configuration
- var firebaseConfig = {
-    apiKey: "AIzaSyCgDcRzJQovwfygPEaDNkIafp82RGAploY",
-    authDomain: "let-chat-web-app-220f5.firebaseapp.com",
-    databaseURL: "https://let-chat-web-app-220f5-default-rtdb.firebaseio.com",
-    projectId: "let-chat-web-app-220f5",
-    storageBucket: "let-chat-web-app-220f5.appspot.com",
-    messagingSenderId: "29419020013",
-    appId: "1:29419020013:web:d66d30567b984808c2b6e4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCgDcRzJQovwfygPEaDNkIafp82RGAploY",
+  authDomain: "let-chat-web-app-220f5.firebaseapp.com",
+  databaseURL: "https://let-chat-web-app-220f5-default-rtdb.firebaseio.com",
+  projectId: "let-chat-web-app-220f5",
+  storageBucket: "let-chat-web-app-220f5.appspot.com",
+  messagingSenderId: "29419020013",
+  appId: "1:29419020013:web:d66d30567b984808c2b6e4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  function addRoom(){
-    
-  }
+function greetUser() {
+  nameOfTheUser = localStorage.getItem("userName");
+  document.getElementById("user_name").innerHTML = "Welcome " + nameOfTheUser + "!!"
+}
+
+function addRoom() {
+  room_name = document.getElementById("room_name").value;
+  firebase.database().ref("/").child(room_name).update({
+    purpose: "adding room"
+  });
+  localStorage.setItem("room_name", room_name);
+}
+
+function redirectToRoomName(roomName){
+  localStorage.setItem("room_name",roomName);
+  window.location = "kwitter_page.html";
+}
+
+function getData() {
+
+  firebase.database().ref("/").on('value', function (snapshot) {
+    document.getElementById("output").innerHTML = "";
+    snapshot.forEach(function (childSnapshot) {
+      childKey = childSnapshot.key; 
+      Room_name = childKey;
+      //Start code    
+
+      row = "<div class='room_name' id=" + Room_name + " onclick='redirectToRoomName(this.id)' >#" + Room_name + "<div/>" + "<hr>";
+      document.getElementById("output").innerHTML+=row;
+
+      //End code      
+    });
+  });
+}
+
+getData();
